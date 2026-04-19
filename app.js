349a4cabe79b7bb4c9888386a3bf8621d0454ab4
ajinkya2304 <159646180+ajinkya2304@users.noreply.github.com>
2026-04-19 10:28:23 +0530

    added some changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("🚀 Node.js app running with PM2! working for learning purpose");
+  res.send("🚀 Node.js app running with PM2! working for learning purpose add other line");
 });
 
 app.get("/health", (req, res) => {
