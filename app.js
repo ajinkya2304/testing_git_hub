5150d9875713e3a4834edc66d9f796d3586a398e
ajinkya2304 <159646180+ajinkya2304@users.noreply.github.com>
2026-04-21 10:29:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("🚀 , testing first commit v1. inal testing.new one,checking conditional statement,t, we are testing our new server ");
+  res.send("🚀 , Hii ajinkya your cicd testing is successful ");
 });
 
 app.get("/health", (req, res) => {
